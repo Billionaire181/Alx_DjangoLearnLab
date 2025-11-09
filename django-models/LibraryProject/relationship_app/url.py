77fa51820b717e547e079b165f7e426129e81f42
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-11-09 23:41:30 +0100

    Error handled

--- django-models/LibraryProject/relationship_app/url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import List_books, LibraryDetailView
-
-urlpatterns = [
-	path ('books/', book_list, name='list_books'),
-	path ('library/<int:pk>/', LibraryDetailView.as_view(), name = 'library_detail'),
-]
